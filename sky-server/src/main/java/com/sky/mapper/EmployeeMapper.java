da72cef6901c02bf181c8ffcdd44fffad564ad63
GHR <2936266854@qq.com>
2024-07-23 09:48:34 +0800

    公共字段自动填充

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -25,6 +27,8 @@ public interface EmployeeMapper {
             "values " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber}," +
             "#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @AutoFill(value=OperationType.INSERT)
+
     void insert(Employee employee);
 
     /**
@@ -40,6 +44,7 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
+    @AutoFill(value=OperationType.UPDATE)
     void update(Employee employee);
 
     /**
